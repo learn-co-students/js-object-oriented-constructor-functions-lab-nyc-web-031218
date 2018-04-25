1d57031dedeb7a5cc189dd664c8e955c1dba2b97
Al <lin.alice72@gmail.com>
2018-04-25 12:15:00 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(year, color, model){
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+function Driver(name, age, experience){
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(addr, ct){
+  this.address = addr;
+  this.city = ct;
+}
