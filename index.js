cd022af74cc21602642889ddf5d8f7e3fa9a4770
Rani Shah <rani.shah@flatironschool.com>
2018-04-25 09:42:43 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var Scooter = function (year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+var Driver = function (name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+var PickupLocation = function (address, city) {
+  this.address = address
+  this.city = city
+}
